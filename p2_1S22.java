88c0b1f3bcece4c56614b8b2c2a4d26f37488f00
juanpabloortizgarcia <144287735+juanpabloortizgarcia@users.noreply.github.com>
2023-09-06 19:21:48 -0600

    Add files via upload

---
+++ p2_1S22.java
@@ -0,0 +1,6 @@
+class P2_1s22{public static void main(String[]args){
+System.out.println("Juan Pablo Lorenzo Ortiz Garcia");
+System.uot.println("edad:18 años");
+System.out.println("ortizjuanpablo834@gmail.com");
+System.out.println("me encanta el basquetball");
+System.out.println("mi color favorito es el anaranjado");∫
