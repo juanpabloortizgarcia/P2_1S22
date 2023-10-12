e7888e8eddd0c7697f5f80f30c1b934714330df0
juanpabloortizgarcia <144287735+juanpabloortizgarcia@users.noreply.github.com>
2023-10-11 18:39:07 -0600

    Delete p2/ p2_1s21.java

--- p2/ p2_1s21.java
+++
@@ -1,8 +0,0 @@
-class p2_1s21{
-	public static void main(String[] args) {
-		System.out.println("Datos personales");
-		System.out.println("nombre:Eduardo Rodrigo Reyna Bello")
-		System.out.println("edad: 18 años")
-		System.out.println("correo: rodrigoreynahaddo@gmail.com")
-		System.out.println("aficion: jugar")
-		System.out.println("color favorito: azul") 
