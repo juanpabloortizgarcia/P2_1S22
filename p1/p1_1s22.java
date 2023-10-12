740b38925549886c12c41095040bcf7cbba4005a
juanpabloortizgarcia <144287735+juanpabloortizgarcia@users.noreply.github.com>
2023-10-11 18:35:52 -0600

    Add files via upload

---
+++ p1/p1_1s22.java
@@ -0,0 +1,6 @@
+class p1_1s22{
+	public static void main(String[] args) {
+		System.out.println("hola");
+		System.out.println("Hola por 2");
+	}
+}
---
+++ p1/p2_1s22.java
@@ -0,0 +1,9 @@
+public class p2_1s22{
+	public static void main(String[] args) {
+		System.out.println("Nombre:Angel Sebastian Baltazar Mijangos");
+		System.out.println("edad:17 Años");
+		System.out.println("Correo:angelsebastianbaltazar253@gmail.com");
+		System.out.println("Aficion:los videojuegos y el futbol");
+		System.out.println("Color:Negro");
+	}
+}
