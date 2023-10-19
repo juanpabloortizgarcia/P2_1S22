f5dbfe57fd550650523fea096f19f2fbc3c8f8d7
juanpabloortizgarcia <144287735+juanpabloortizgarcia@users.noreply.github.com>
2023-10-18 19:21:47 -0600

    Add files via upload

---
+++ p2/p2p7_1s22.java
@@ -0,0 +1,8 @@
+public class p2p7_1s22{
+	public static void main(String[] args){
+		System.out.println("numero de parametros ingresados desde linea de comando "+ args.length);
+		for(int a=0;a<args.length;a++){
+			System.out.println("valor #"+a+ ":"+args[a]);
+		}
+	}
+}
